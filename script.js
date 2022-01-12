2cb960490bc944afc85ff5a41287c9587d5bec1e
Tahseen <bhat.tahseen8@gmail.com>
2022-01-12 16:51:28 +0530

    Added HighestScore

--- script.js
+++ script.js
@@ -7,18 +7,35 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').innerHTML = secretNumber;
+let highestScore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   //   console.log(guess, typeof guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').innerHTML = '⛔ No Number!';
-  } else if (guess === secretNumber) {
+  }
+
+  // When player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').innerHTML = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
+    document.querySelector('.number').innerHTML = secretNumber;
+
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highestScore) {
+      highestScore = score;
+      console.log(highestScore);
+      document.querySelector('.highscore').innerHTML = highestScore;
+    }
+  }
+
+  // When guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').innerHTML = 'Too High!';
       score--;
@@ -28,7 +45,10 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.score').innerHTML = 0;
       document.body.style.backgroundColor = 'red';
     }
-  } else if (guess < secretNumber) {
+  }
+
+  // When guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').innerHTML = 'Too Low!';
       score--;
